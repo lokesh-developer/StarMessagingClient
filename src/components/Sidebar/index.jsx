1b1baa23a7d6ae23608974d7a87a173550c5ef41
Lokesh Kabra <64826892+lokesh-developer@users.noreply.github.com>
2021-08-06 18:12:07 +0530

    now perfect logout :)

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -25,8 +25,8 @@ export const Sidebar = ({ Open, Close }) => {
   const value = useColorModeValue("dark", "light");
   const bg = useColorModeValue("gray.100", "gray.800");
   const history = useHistory();
-  const logoutinSuccess = () => {
-    localStorage.removeItem("profile");
+  const logoutSuccess = () => {
+    localStorage.clear();
     history.push("/");
   };
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -63,7 +63,7 @@ export const Sidebar = ({ Open, Close }) => {
                 theme={value}
                 buttonText="Logout"
                 clientId="1045205020440-m237oug228ipsn2ro025hjlb8148ggge.apps.googleusercontent.com"
-                onLogoutSuccess={logoutinSuccess}
+                onLogoutSuccess={logoutSuccess}
               />
             </Flex>
           </DrawerBody>
